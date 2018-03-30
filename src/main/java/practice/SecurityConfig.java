4a0b0fd5ed56f6afc77a3b4f121ea49362b47b63
hue9010 <hue9010@gmail.com>
2018-03-30 19:11:29 +0900

    [add]구글 로그인 구현

--- src/main/java/practice/SecurityConfig.java
+++ src/main/java/practice/SecurityConfig.java
@@ -1,7 +1,14 @@
 package practice;
 
 import javax.annotation.Resource;
+import javax.servlet.Filter;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,12 +17,21 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import practice.model.MemberRole;
 import practice.service.SecurityMemberService;
 
 @Configuration
 @EnableWebSecurity
+@EnableOAuth2Client
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Resource(name="securityMemberService")
 	private SecurityMemberService securityMemberService;
@@ -30,11 +46,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   		http.csrf().disable();
   		http.headers().frameOptions().disable();
   		http.httpBasic();
-  		
-		http
-			.authorizeRequests()
-			.antMatchers("/users/list").hasRole(MemberRole.USER)
-			.antMatchers("/**").permitAll();
+
+  		http
+		.authorizeRequests()
+		.antMatchers("/users/list").hasRole(MemberRole.USER)
+		.antMatchers("/**").permitAll()
+		.and().addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
 		
   		http
 			.formLogin()
@@ -52,4 +69,40 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
 	  auth.userDetailsService(securityMemberService).passwordEncoder(passwordEncoder());
 	}
+	
+	@Autowired
+	private OAuth2ClientContext oauth2ClientContext;
+
+	@Bean
+	public Filter ssoFilter() {
+		OAuth2ClientAuthenticationProcessingFilter oauth2Filter = new OAuth2ClientAuthenticationProcessingFilter(
+				"/users/login/google");
+		OAuth2RestTemplate oAuth2RestTemplate = new OAuth2RestTemplate(googleClient(), oauth2ClientContext);
+		oauth2Filter.setRestTemplate(oAuth2RestTemplate);
+		oauth2Filter.setTokenServices(
+				new UserInfoTokenServices(googleResource().getUserInfoUri(), googleClient().getClientId()));
+
+		return oauth2Filter;
+	}
+
+	@Bean
+	@ConfigurationProperties("google.client")
+	public OAuth2ProtectedResourceDetails googleClient() {
+		return new AuthorizationCodeResourceDetails();
+	}
+
+	@Bean
+	@ConfigurationProperties("google.resource")
+	public ResourceServerProperties googleResource() {
+		return new ResourceServerProperties();
+	}
+
+	@Bean
+	public FilterRegistrationBean oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
+		FilterRegistrationBean registration = new FilterRegistrationBean();
+		registration.setFilter(filter);
+		registration.setOrder(-100);
+		return registration;
+	}
+
 }
